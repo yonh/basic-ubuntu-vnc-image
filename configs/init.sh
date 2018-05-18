c59338466f50ce0e1e5253d74c03305789379a03
yonh <azssjli@163.com>
2018-05-18 14:56:43 +0800

    anthoer way to get current folder path

--- configs/init.sh
+++ configs/init.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 CURR_DIR=$(cd "$(dirname "$0")";pwd)
+#CURR_DIR=$(dirname $(readlink -f "$0"))
 cd $CURR_DIR
 
 mkdir -p /root/.config/fish/
@@ -9,5 +10,6 @@ mkdir -p /root/.ssh/
 
 mv config.fish /root/.config/fish/config.fish
 mv authorized_keys /root/.ssh/authorized_keys
+
 
 
